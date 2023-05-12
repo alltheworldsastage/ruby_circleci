52bdbefba90876915a5d3c336b3649e51f551902
alltheworldsastage <slattery@gmail.com>
2023-05-12 17:35:28 +1000

    Finish coveralls setup.

--- Gemfile
+++ Gemfile
@@ -9,3 +9,4 @@ git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 gem 'rspec'
 gem 'simplecov'
 gem 'rspec_junit_formatter'
+gem 'simplecov-lcov'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,10 @@
 
 
 require 'simplecov'
+require 'simplecov-lcov'
+
+SimpleCov::Formatter::LcovFormatter.config.report_with_single_file = true
+SimpleCov.formatter = SimpleCov::Formatter::LcovFormatter
 
 SimpleCov.start do
   add_filter "/spec/"
